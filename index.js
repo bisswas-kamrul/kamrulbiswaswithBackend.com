e9337acf93b0ecca903c914684fdd7d2fbcf3149
bisswas-kamrul <bisswaskamruldev@gmail.com>
2026-06-25 17:04:21 +0600

    faaa

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
       "https://kamrulbiswas-com.vercel.app",
+      "https://kamrulbiswaswithbackend-com.onrender.com",
     ],
     credentials: true,
   })
